87b35769d643f50d3f3f7c481da8bd83462af7b2
Nugumanov <>
2018-07-31 13:56:12 +0300

    first

---
+++ SpalahBook_9.2/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SpalahBook_9.2
+//
+//  Created by Nugumanov on 31.07.18.
+//  Copyright © 2018 Nugumanov Dima. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ SpalahBook_9.2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpalahBook_9.2
+//
+//  Created by Nugumanov on 31.07.18.
+//  Copyright © 2018 Nugumanov Dima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
